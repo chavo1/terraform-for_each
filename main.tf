3656f35db46d81e34fd8ebbf91976409d8a1593d
chavo1 <chavdar.rakov@gmail.com>
2019-11-17 20:42:10 +0200

    shuffle

--- main.tf
+++ main.tf
@@ -19,3 +19,26 @@ resource "null_resource" "server" {
 resource "null_resource" "server1" {
   for_each = tomap(local.my_map)
 }
+
+# For Expressions
+variable "ip" {
+  description = "List of ips"
+  default     = ["192.168.0.1", "192.168.0.2", "192.168.0.3", "192.168.0.4", "192.168.0.5"]
+}
+
+resource "random_shuffle" "ip" {
+  input = [
+    for ip in var.ip :
+    chomp(ip) // chomp removes newline characters at the end of a string.
+  ]
+}
+
+# Splat
+variable "ip_splat" {
+  description = "List of ips"
+  default     = ["192.168.0.6", "192.168.0.7", "192.168.0.8", "192.168.0.9", "192.168.0.10"]
+}
+
+resource "random_shuffle" "ip_splat" {
+  input = var.ip_splat
+}
--- output.tf
+++ output.tf
@@ -5,3 +5,11 @@ output "ids" {
 output "ids1" {
   value = values(null_resource.server1)[*].id
 }
+
+output "shuffled_output" {
+  value = random_shuffle.ip.result
+}
+
+output "shuffled_output_splat" {
+  value = "${random_shuffle.ip_splat.*.result}"
+}
